2beb9c377dcd94e1293b2ce83f36c74cb16cf48b
blancojohn <142819007+blancojohn@users.noreply.github.com>
2023-11-29 17:34:53 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excusesRandom();
+};
+
+let excusesRandom = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoI = Math.floor(Math.random() * who.length);
+  let actionI = Math.floor(Math.random() * action.length);
+  let whatI = Math.floor(Math.random() * what.length);
+  let whenI = Math.floor(Math.random() * when.length);
+  return (
+    who[whoI] + " " + action[actionI] + " " + whatI[whatI] + " " + when[whenI]
+  );
 };
